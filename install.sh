abe9538d1d13537dbb3dea929a0c11c40b058fc7
yugo1103 <y1103orz@akane.waseda.jp>
2019-10-30 15:57:50 +0900

    install.sh

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -u
+DOT_DIRECTORY="${HOME}/dotfiles"
+DOT_CONFIG_DIRECTORY=".config"
+
+echo "link home directory dotfiles"
+cd ${DOT_DIRECTORY}
+for f in .??*
+do
+    #無視したいファイルやディレクトリ
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".config" ] && continue
+    ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+done
+
+echo "link .config directory dotfiles"
+cd ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}
+for file in `\find . -maxdepth 8 -type f`; do
+#./の2文字を削除するためにfile:2としている
+    ln -snfv ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}/${file:2} ${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}
+done
+
+echo "linked dotfiles complete!"
+
+
+
+packagelist=(
+#terminal
+     "tmux"
+)
+
+echo "start apt-get install apps..."
+for list in ${packagelist[@]}; do
+    sudo apt-get install -y ${list}
+done
+
+sudo apt-get update
+sudo apt-get upgrade
